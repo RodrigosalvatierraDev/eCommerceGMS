b21f8d94883e8cdd5cfffe0b8fea27c39793d758
RodrigosalvatierraDev <RodrigosalvatierraDev@gmail.com>
2024-12-11 19:02:53 -0300

    Update userApi.js

--- frontend/src/components/api/userApi.js
+++ frontend/src/components/api/userApi.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://localhost:5000/api/users';
+const BASE_URL = `${import.meta.env.VITE_API_URL}/api/users`;
 
 export const getUsers = async () => {
   const response = await fetch(BASE_URL);
